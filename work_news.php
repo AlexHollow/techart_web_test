a06c50466e6a61f5bd75cacb07bd11fdab5b5011
Alex Rud <lexa.roach@gmail.com>
2025-04-16 20:52:47 +0300

    feat: добавить статическую верстку

---
+++ work_news.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галактический вестник</title>
+    <link rel="icon" href="./images/logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="./vendor/normalize.css">
+    <link rel="stylesheet" href="./work_news.css">
+</head>
+<body class="page">
+    <header class="header header_type_work-news">
+        <div class="wrapper">
+            <div class="logo">
+                <img class="logo__img" src="./images/logo.svg" alt="Логотип">
+                <p class="logo__text">Галактический вестник</p>
+            </div>
+        </div>
+    </header>
+
+    <main class="content">
+        <section class="news news_type_work-news">
+            <div class="wrapper">
+                <div class="news__ref-container">
+                    <p class="news__ref-text"><a class="news__ref" href="./index.php">Главная / </a>Возвращение енота</p>
+                </div>
+
+                <h2 class="news__title">Новости</h2>
+
+                <ul class="news__container">
+                    <li>
+                        <div class="card" href="./index.php">
+                            <div class="card__time">20.05.2412</div>
+                            <h3 class="card__title">На чемпионате по пустотной гребле победила команда с Фобоса</h3>
+                            <p class="card__announce">
+                                Сегодня с Проксимы вернулась этнографическая экспедиция под управлением профессора Джона Голдрама из Майхартского университета. Экспедиция работала в джунглях Туманного континента и занималась изучением неконтактного племени двухвостых зюзюмов.
+                            </p>
+                            <p class="card__announce">
+                                "Это племя называет себя Ракатука-Тум-Тум и крайне неохотно контактирует с пришельцами", - сказал Джон Голдрам. На вопрос о том, каковы они - неконтактные зюзюмы, профессор заявил, что неконтактные зюзюмы такие же двухвостые и скандальные как и контактные, только неконтактные.
+                            </p>
+                            <p class="card__announce">
+                                Ждем от профессора Голдрама новых интересных подробностей!
+                            </p>
+                            <a href="./index.php" class="button button_size_large">
+                                <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg" version="1.1"
+                                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    transform="matrix(-1,1.2246467991473532e-16,-1.2246467991473532e-16,-1,0,0)">
+                                    <path
+                                        d="M3 10C2.44772 10 2 10.4477 2 11C2 11.5523 2.44772 12 3 12L3 10ZM18.466 11.7071C18.8565 11.3166 18.8565 10.6834 18.466 10.2929L12.102 3.92893C11.7115 3.53841 11.0783 3.53841 10.6878 3.92893C10.2973 4.31946 10.2973 4.95262 10.6878 5.34315L16.3447 11L10.6878 16.6569C10.2973 17.0474 10.2973 17.6805 10.6878 18.0711C11.0783 18.4616 11.7115 18.4616 12.102 18.0711L18.466 11.7071ZM3 12L17.7589 12L17.7589 10L3 10L3 12Z"
+                                        fill="#841844"></path>
+                                </svg>
+                                &nbsp;Назад к новостям
+                            </a>
+                        </div>
+                    </li>
+
+                    <li>
+                        <img class="news__img" src="./images/3527a1e3c481e1a86495b3d3f431aab1.jpg" alt="Фото из новости">
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
+    
+    <div class="wrapper">
+        <footer class="footer">
+            <p class="footer__text">&copy; 2023 — 2412 «Галактический вестник»</p>
+        </footer>
+    </div>
+</body>
+</html>
