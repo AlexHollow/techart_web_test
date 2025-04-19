876cc86510b86db333d7be0bfd63763af22952d0
Alex Rud <lexa.roach@gmail.com>
2025-04-19 21:05:22 +0300

    fix: исправить кнопки перехода на первую и последнюю страницу

--- components/nav-menu.php
+++ components/nav-menu.php
@@ -20,7 +20,7 @@ $next_page = $current_page + 1;
     <?php 
 
     if ($current_page > 1) {
-        echo('<a class="button button_size_large" href="./index.php?page=' . $prev_page . '">
+        echo('<a class="button button_size_large" href="./index.php?page=1">
             <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" transform="matrix(-1,1.2246467991473532e-16,-1.2246467991473532e-16,-1,0,0)">
                 <path d="M3 10C2.44772 10 2 10.4477 2 11C2 11.5523 2.44772 12 3 12L3 10ZM18.466 11.7071C18.8565 11.3166 18.8565 10.6834 18.466 10.2929L12.102 3.92893C11.7115 3.53841 11.0783 3.53841 10.6878 3.92893C10.2973 4.31946 10.2973 4.95262 10.6878 5.34315L16.3447 11L10.6878 16.6569C10.2973 17.0474 10.2973 17.6805 10.6878 18.0711C11.0783 18.4616 11.7115 18.4616 12.102 18.0711L18.466 11.7071ZM3 12L17.7589 12L17.7589 10L3 10L3 12Z" fill="#841844"></path>
             </svg>
@@ -54,7 +54,7 @@ $next_page = $current_page + 1;
 
     if ($current_page != $max_pages_count) {
         echo('
-        <a class="button button_size_large" href="./index.php?page=' . $next_page . '">
+        <a class="button button_size_large" href="./index.php?page=' . $max_pages_count . '">
             <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M3 10C2.44772 10 2 10.4477 2 11C2 11.5523 2.44772 12 3 12L3 10ZM18.466 11.7071C18.8565 11.3166 18.8565 10.6834 18.466 10.2929L12.102 3.92893C11.7115 3.53841 11.0783 3.53841 10.6878 3.92893C10.2973 4.31946 10.2973 4.95262 10.6878 5.34315L16.3447 11L10.6878 16.6569C10.2973 17.0474 10.2973 17.6805 10.6878 18.0711C11.0783 18.4616 11.7115 18.4616 12.102 18.0711L18.466 11.7071ZM3 12L17.7589 12L17.7589 10L3 10L3 12Z" fill="#841844"/>
             </svg>
